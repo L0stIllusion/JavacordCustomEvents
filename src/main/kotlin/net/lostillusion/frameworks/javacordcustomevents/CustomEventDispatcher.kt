7c0b93316c52376e9b06484e2b32407e26cc7949
Lost Illusion <zctrap@gmail.com>
2019-06-29 05:14:30 -0400

    Allow for sub classes of events.

--- src/main/kotlin/net/lostillusion/frameworks/javacordcustomevents/CustomEventDispatcher.kt
+++ src/main/kotlin/net/lostillusion/frameworks/javacordcustomevents/CustomEventDispatcher.kt
@@ -2,7 +2,7 @@ package net.lostillusion.frameworks.javacordcustomevents
 
 import org.javacord.api.event.Event
 
-abstract class CustomEventDispatcher<I: Event, O: CustomEvent<I>>(val inputEventClass: Class<I>) {
+abstract class CustomEventDispatcher<I: Event, O: CustomEvent<out I>>(val inputEventClass: Class<I>) {
     private val eventListeners: MutableMap<String, CustomEventListener<O>> = mutableMapOf()
     protected abstract val eventFilter: (event: I) -> Boolean
     protected abstract val eventTransformer: (event: I) -> O
